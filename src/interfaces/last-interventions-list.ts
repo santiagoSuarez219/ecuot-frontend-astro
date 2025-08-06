58c90f8946c152786e382ab78fde205fd44ea8c3
santiagoSuarez219 <santiago8628@gmail.com>
2025-08-05 20:49:55 -0500

    creating home view

--- src/interfaces/last-interventions-list.ts
+++ src/interfaces/last-interventions-list.ts
@@ -1,1 +1,23 @@
-export interface LastInterventionsList {}
+export interface LastInterventionsListResponse {
+    _id:              string;
+    interventionName: string;
+    description:      string;
+    hierarchy:        string;
+    strategicProject: string;
+    internalSystem:   InternalSystem;
+    image:            string;
+    datasheet:        string;
+    conflicts:        string[];
+    news:             string[];
+    createdAt:        Date;
+    updatedAt:        Date;
+    __v:              number;
+}
+
+export interface InternalSystem {
+    _id:        string;
+    systemName: string;
+    createdAt:  Date;
+    updatedAt:  Date;
+    __v:        number;
+}
